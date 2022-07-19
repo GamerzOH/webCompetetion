c028a771620e57b535c15e92c3407673082f12fe
GamerzOH <scripterzofindia@gmail.com>
2022-07-19 16:28:49 +0530

    update part 2

---
+++ prototype-2/app.js
@@ -0,0 +1,18 @@
+const highlighterElem = document.querySelector('nav .highlighter-elem');
+const menuItems = document.querySelectorAll('nav a.links');
+
+function scrollObserver() {
+	let current = 'home';
+	[...navSections].forEach(section => {
+		if (scrollY >= section.offsetTop - section.clientHeight / 3)
+			current = section.id;
+	});
+	[...menuItems].forEach(element => {
+		element.classList.remove('active');
+		if (element.classList.contains(current)) {
+			element.classList.add('active');
+		}
+	});
+}
+
+window.onscroll = scrollObserver;
